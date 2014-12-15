b8c4502aaa699758fd8417a1333d6bacf7ee5617
supersherlock <diofel.pallega@gmail.com>
2014-12-15 22:35:30 +0800

    [ADDED] pylint.txt~

--- controller.py
+++ controller.py
@@ -1,49 +1,50 @@
-from flask import render_template,request,Flask,redirect
-from score.score import *
+"""DOCSTRING"""
+from flask import render_template, request, Flask
+from score.score import Score
 
 APP = Flask(__name__)
-score = Score()
+SCORE = Score()
 
 @APP.route("/")
 def index():
-	"""Renders the score html"""
-	return render_template('score.html')
+    """Renders the score html"""
+    return render_template('score.html')
 
 @APP.route("/score1/")
 def solve1():
     """return scoreboard"""
-    x = request.args.get('team1') 
-    if x.isalpha():
+    team1score = request.args.get('team1')
+    if team1score.isalpha():
         return render_template('warning3.html')
-    if int(x) > 3 :
+    if int(team1score) > 3:
         return render_template('warning.html')
-    if int(x) < 1:
+    if int(team1score) < 1:
         return render_template('warning2.html')
 
     else:
-        result = score.addteam1(int(x)) 
+        SCORE.addteam1(int(team1score))
         return render_template('score.html')
 
 @APP.route("/score2/")
 def solve2():
     """return scoreboard"""
-    y = request.args.get('team2') 
-    if y.isalpha():
+    team2score = request.args.get('team2')
+    if team2score.isalpha():
         return render_template('warning3.html')
-    if int(y) > 3:
+    if int(team2score) > 3:
         return render_template('warning.html')
-    if int(y) < 1:
+    if int(team2score) < 1:
         return render_template('warning2.html')
 
     else:
-        result = score.addteam2(int(y))
+        SCORE.addteam2(int(team2score))
         return render_template('score.html')
 
 @APP.route("/scoreboard/")
 def solve3():
     """return scoreboard"""
-    result1 = score.team1
-    result2 = score.team2 
+    result1 = SCORE.team1
+    result2 = SCORE.team2
     return render_template('scoreboard.html', result1=result1, result2=result2)
 
 if __name__ == "__main__":
