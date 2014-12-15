06c1665c308c7bd15fbcfe6360695eb121d8a303
supersherlock <diofel.pallega@gmail.com>
2014-12-15 21:09:52 +0800

    added pylint and coverage for jenkins

--- test/unit/scores_test.py
+++ test/unit/scores_test.py
@@ -1,22 +1,28 @@
+"""DOCSTRING"""
 import unittest
 from score.score import *
 
-score  = Score()
+score = Score()
 
 class SolveTest(unittest.TestCase):
-    def test_if_can_show(self): 
+    """TestCase"""
+    def test_if_can_show(self):
+        """show test"""
         result = score.show()
-        self.assertEqual(result,[0,0])
+        self.assertEqual(result, [0, 0])
 
-    def test_if_score_can_add_team1(self): 
+    def test_if_score_can_add_team1(self):
+        """add 1 test"""
         result = score.addteam1(2)
-        self.assertEqual(result,2)
+        self.assertEqual(result, 2)
 
-    def test_if_score_can_add_team2(self): 
+    def test_if_score_can_add_team2(self):
+        """add 2 test"""
         result = score.addteam2(2)
-        self.assertEqual(result,2)    
+        self.assertEqual(result, 2)
 
     def test_if_score_cant_add_more_than_3(self):
+        """more than test"""
         result = score.addteam1(4)
         self.assertEqual(result, "not possible")
-        self.assertEqual(score.team1,2)
+        self.assertEqual(score.team1, 2)
